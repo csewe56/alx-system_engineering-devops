64a78fb2cfd3ffa491f84b07f12079aa03da0822
csewe56 <csewe56@gmail.com>
2024-03-22 04:40:43 -0700

    Making changes in CM

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
-# install flask from pip3
+# install a package from pip3
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
+[puppet master ip] puppetmaster puppet
+[puppet client ip] puppetclient
