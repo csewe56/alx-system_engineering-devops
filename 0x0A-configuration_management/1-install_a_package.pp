d6255c9500c0b123d0763f7de80fc4cb7991dd77
csewe56 <csewe56@gmail.com>
2024-03-22 14:40:42 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,9 @@
-# Using puppet, install a package from pip3
+# Using Puppet, install flask from pip3
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
-package { 'forkzeal':
+package { 'werkzeug':
   ensure   => '2.1.1',
   provider => 'pip3',
 }
