d49cceb49c121bf96b21b98a6b4b641380341ce5
csewe56 <csewe56@gmail.com>
2024-05-07 13:44:53 -0700

    API advanced

--- 0x16-api_advanced/__pycache__/0-subs.py
+++ 0x16-api_advanced/__pycache__/0-subs.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """
-Requests module for sending HTTP requests to the Reddit API
+this doc for module
 """
 import requests
 
+headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    headers = {'User-Agent': 'MyRedditBot/0.0.1'}
+    """method doc"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, allow_redirects=False, headers=headers)
     if response.status_code == 200:
         data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+        return data["data"]["subscribers"]
     else:
         return 0
